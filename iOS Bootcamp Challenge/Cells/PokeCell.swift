c2a6d49688d28801931e40f6e72c7f7e32885ae0
Jorge Benavides <jojinaga@evidation.com>
2021-09-27 23:15:04 -0500

    Displaying poke types on card

--- iOS Bootcamp Challenge/Cells/PokeCell.swift
+++ iOS Bootcamp Challenge/Cells/PokeCell.swift
@@ -26,7 +26,26 @@ class PokeCell: UICollectionViewCell {
         view.textColor = UIColor.white
         return view
     }()
-
+    
+    lazy private var typesContainer: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.clipsToBounds = true
+        return view
+    }()
+    
+    private func createTypeLabel(name: String) -> UILabel {
+        let view = UILabel()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.font = .boldSystemFont(ofSize: 12)
+        view.textColor = .white.withAlphaComponent(0.8)
+        view.text = " \(name.capitalized) "
+        view.backgroundColor = .white.withAlphaComponent(0.2)
+        view.layer.cornerRadius = 6
+        view.clipsToBounds = true
+        return view
+    }
+    
     lazy private var imageView: UIImageView = {
         let view = UIImageView()
         view.translatesAutoresizingMaskIntoConstraints = false
@@ -42,7 +61,7 @@ class PokeCell: UICollectionViewCell {
         view.alpha = 0.1
         return view
     }()
-
+    
     private var gradient: CAGradientLayer?
 
     // general margin for ui elements
@@ -55,25 +74,23 @@ class PokeCell: UICollectionViewCell {
         super.init(coder: aDecoder)
         backgroundColor = .blue.withAlphaComponent(0.3)
         layer.cornerRadius = 16
-        layer.shadowColor = UIColor.black.cgColor
-        layer.shadowOpacity = 1
-        layer.shadowOffset = .zero
-        layer.shadowRadius = 3
-
+        
         setupUI()
     }
 
     private func setupUI() {
         addSubview(backgroundBall)
+        
         backgroundBall.topAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
         backgroundBall.leftAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
         backgroundBall.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
         backgroundBall.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
-
+        
         addSubview(idLabel)
         addSubview(nameLabel)
         addSubview(imageView)
-
+        addSubview(typesContainer)
+        
         idLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: margin).isActive = true
         idLabel.leftAnchor.constraint(equalTo: self.leftAnchor, constant: margin).isActive = true
         idLabel.widthAnchor.constraint(lessThanOrEqualTo: self.widthAnchor).isActive = true
@@ -86,6 +103,11 @@ class PokeCell: UICollectionViewCell {
         imageView.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -offset).isActive = true
         imageView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -offset).isActive = true
         imageView.leftAnchor.constraint(equalTo: nameLabel.rightAnchor, constant: margin).isActive = true
+        
+        typesContainer.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: margin).isActive = true
+        typesContainer.leftAnchor.constraint(equalTo: self.leftAnchor, constant: margin).isActive = true
+        typesContainer.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -margin).isActive = true
+        typesContainer.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -margin).isActive = true
     }
 
     override func prepareForReuse() {
@@ -95,6 +117,7 @@ class PokeCell: UICollectionViewCell {
         nameLabel.text = nil
         imageView.image = nil
         gradient?.removeFromSuperlayer()
+        typesContainer.subviews.forEach { $0.removeFromSuperview() }
     }
 
     var pokemon: Pokemon? {
@@ -106,7 +129,20 @@ class PokeCell: UICollectionViewCell {
             if let image = pokemon.image, let url = URL(string: image) {
                 imageView.kf.setImage(with: url)
             }
+            
+            for type in pokemon.types ?? [] {
+                let last = typesContainer.subviews.last
+                let view = createTypeLabel(name: type)
+                typesContainer.addSubview(view)
 
+                if let last = last {
+                    view.leftAnchor.constraint(equalTo: last.rightAnchor, constant: 4).isActive = true
+                } else {
+                    view.leftAnchor.constraint(equalTo: typesContainer.leftAnchor).isActive = true
+                }
+                view.bottomAnchor.constraint(equalTo: typesContainer.bottomAnchor).isActive = true
+            }
+            
             gradient = PokemonColor.typeLinearGradient(name: pokemon.primaryType())
             guard let gradient = gradient else { return }
             gradient.frame = bounds
