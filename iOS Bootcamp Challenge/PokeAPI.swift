5716231fff14197b3b22a467ff56be1739b427f4
Marco Chavez <marco.chavez@globant.com>
2021-10-07 19:20:53 -0500

    TODO: Implements generics to merge this methods into one

--- iOS Bootcamp Challenge/PokeAPI.swift
+++ iOS Bootcamp Challenge/PokeAPI.swift
@@ -51,4 +51,24 @@ class PokeAPI {
         task?.resume()
         return task
     }
+
+    @discardableResult
+    func get<T: Decodable>(url: String, onCompletion: @escaping(T?, Error?) -> Void) -> URLSessionDataTask? {
+        let path = url.replacingOccurrences(of: PokeAPI.baseURL, with: "")
+        let task = URLSession.mock.dataTask(with: PokeAPI.baseURL + path, completionHandler: { data, _, error in
+            guard let data = data else {
+                onCompletion(nil, error)
+                return
+            }
+            do {
+                let entity = try JSONDecoder().decode(T.self, from: data)
+                onCompletion(entity, error)
+            } catch {
+                onCompletion(nil, error)
+            }
+        })
+        task?.resume()
+        return task
+    }
+
 }
